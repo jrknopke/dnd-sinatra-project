91a7aaffcb6368478cacc76b70fb96e3ecc3c7f2
Joseph Knopke <josephknopke@gmail.com>
2021-02-13 15:24:44 -0600

    basic routes

--- app/controllers/spells_controller.rb
+++ app/controllers/spells_controller.rb
@@ -3,34 +3,52 @@ class SpellsController < ApplicationController
     # CREATE
 
         # New
-        # make a get request to '/characters/new'
         get '/spells/new' do
             erb :'/spells/new'
         end
 
         # Create
-        # make a post request to '/characters'
+        post '/spells' do
+            spell = Spell.new(params)
+            if spell.save
+                redirect '/spells'
+            else
+                erb :'/spells/new'
+            end
+        end
 
     # READ
-        # Index - make a get request to '/characters'
 
         get '/spells' do
-            @spells = Spell.all
+            @spells = Spell.all.reverse
             erb :'spells/index'
         end
 
         # Show - make a get request to '/characters/:id'
-        # get '/spells/:id' do
-        #     @spell = Spell.find
-        #     erb :'spells/show'
-        # end
+        get '/spells/:id' do
+            @spell = Spell.find(params[:id])
+            erb :'spells/show'
+        end
     # UPDATE
 
         # Edit
-        # make a get request to '/characters/:id/edit'
+        get '/spells/:id/edit' do
+            @spell = Spell.find(params[:id])
+            erb :'/spells/edit'
+        end
 
         #Update
-        # make a put request to '/characters/:id'
+        patch '/spells/:id' do
+            @spell = Spell.find(params[:id])
+
+            if !spell.name.empty? && !spell.level.empty?
+                @spell.update(params["spell"])
+                redirect '/spells/#{params[:id]}'
+            else
+                @error = "Invalid entries. Please try again"
+                erb:'/recipes/edit'
+            end
+        end
 
     # DESTROY
 
